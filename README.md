361d4d00e44dd2b1d7a353373d034f00594ba121
gzuuus <gzuuus@protonmail.com>
2023-04-09 17:45:01 +0200

    styles and fixes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ### Nostri.chat chrome extension
 ##### Loads nostrichat everywhere
 This is a work in progress
-- To try it:
+Try it:
 - clone this repo and go to your chrome browser
-- In chrome go to 'chrome://extensions' and push 'Load unpacked'
-- Now you have a new extension that loads nostri.chat everywhere and take the actuar url to set the topic
+- In chrome go to 'chrome://extensions' and press 'Load unpacked'
+- Now you have a new extension that loads nostri.chat everywhere and takes the actual URL to set the topic
+
+For more info about nostr.chat visit https://nostri.chat/
--- popup.js
+++ popup.js
@@ -1,10 +1,36 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var url = tabs[0].url;
     var cleanedUrl = url.replace(/^(https?:\/\/)?(www\.)?/i, ''); // Elimina el prefijo de la URL
-    var scriptCode = `var script = document.createElement("script"); script.src = "https://nostri.chat/public/bundle.js"; script.setAttribute("data-website-owner-pubkey", "40b9c85fffeafc1cadf8c30a4e5c88660ff6e4971a0dc723d5ab674b5e61b451"); script.setAttribute("data-chat-type", "GLOBAL"); script.setAttribute("data-chat-tags","${url}"); script.setAttribute("data-relays", "wss://relay.f7z.io,wss://nos.lol,wss://relay.nostr.info,wss://nostr-pub.wellorder.net,wss://relay.current.fyi,wss://relay.nostr.band"); document.body.appendChild(script); var link = document.createElement("link"); link.rel = "stylesheet"; link.href = "https://nostri.chat/public/bundle.css"; document.body.appendChild(link);`;
+    var scriptCode = `
+      var container = document.getElementById('nostri-container');
+      if (container) {
+        container.remove();
+      }
+      container = document.createElement('div');
+      container.setAttribute('id', 'nostri-container');
+      container.classList.add('${cleanedUrl}');
+      var script = document.createElement('script');
+      script.src = "https://nostri.chat/public/bundle.js";
+      script.setAttribute("data-website-owner-pubkey", "#");
+      script.setAttribute("data-chat-type", "GLOBAL");
+      script.setAttribute("data-chat-tags","${url}");
+      script.setAttribute("data-relays", "wss://relay.f7z.io,wss://nos.lol,wss://relay.nostr.info,wss://nostr-pub.wellorder.net,wss://relay.current.fyi,wss://relay.nostr.band");
+      container.appendChild(script);
+      var link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "https://nostri.chat/public/bundle.css";
+      container.appendChild(link);
+      document.body.appendChild(container);
+      
+      var style = document.createElement("style");
+      style.type = "text/css";
+      style.innerHTML = "#nostri-container { z-index: 9999; }";
+      document.head.appendChild(style);
+    `;
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: scriptCode}
     );
     document.getElementById('url').innerHTML = url;
-});
+  });
+  
